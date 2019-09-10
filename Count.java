d1c44b43137682378237c2d1262cca66f1410172
liang <978460621@qq.com>
2019-09-10 19:30:32 +0800

    new commit

---
+++ Count.java
@@ -0,0 +1,8 @@
+public class Count{
+    public static void main (String [] args){
+		int count = 1;
+		for (int num =100; count<=num; count++ )
+		System.out.println(count)
+    }
+}
+
---
+++ SayHello.java
@@ -0,0 +1,12 @@
+public class SayHello{
+    public static void main(String[] args){
+	    System.out.println("Below is the function of sayhello");
+		sayhello();
+	}    
+	
+	public static void sayhello(int sum){
+	    System.out.println("say hello!"+sum);
+	}
+	
+	
+}
